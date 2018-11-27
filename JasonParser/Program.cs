2c4411e8688f88bc97d911a2340af6d06da29303
KaragozE <karagoz.eray@gmail.com>
2018-11-27 11:40:04 +0300

    first commit

---
+++ JasonParser/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace JasonParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Eray",
+                Degree = "MCA",
+                Hobbies = new System.Collections.Generic.List<string>()
+                {
+                    "Chess",
+                    "Formula 1"
+                }
+
+
+            };
+
+
+            string strResultJson = JsonConvert.SerializeObject(student);
+            System.IO.File.WriteAllText(@"student.json",strResultJson);
+            Console.WriteLine("stored!");
+
+
+            strResultJson = String.Empty;
+            strResultJson = System.IO.File.ReadAllText(@"student.json");
+            //   Student resultStudent = JsonConvert.DeserializeObject<Student>(strResultJson);
+            //   Console.WriteLine(resultStudent.ToString());
+            //   Console.ReadKey();
+
+            var dictionary = JsonConvert.DeserializeObject<IDictionary>(strResultJson);
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                Console.WriteLine(entry.Key+" : "+entry.Value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ JasonParser/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace JasonParser
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+        public List<string> Hobbies { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Student information:\n Id :{0}\n Name :{1} \n Degree : {2}\n Hobbies : {3} ",Id,Name,Degree,string.Join(",",Hobbies.ToArray()));
+        }
+    }
+}
